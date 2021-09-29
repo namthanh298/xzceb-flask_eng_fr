6a98bad3fb22ee4d53df24a57c91b99757a0ee43
namthanh298 <69186819+namthanh298@users.noreply.github.com>
2021-09-29 22:05:19 +0700

    Add files via upload

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,1 +1,16 @@
+import unittest
+from translator import englishToFrench, frenchToEnglish
 
+
+class TestEngToFren(unittest.TestCase):
+    def test1(self):
+        self.assertNotEqual(englishToFrench('I love you'), 'I love you')
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+
+
+class TestFrenToEng(unittest.TestCase):
+    def test1(self):
+        self.assertNotEqual(frenchToEnglish('Je t''aime'), '')
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+
+unittest.main()
